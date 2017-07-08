6a31cbaa5130c88edae969554d8252012c2dea02
ScottSussexGit <SSussexGit@users.noreply.github.com>
2017-07-08 14:32:26 -0400

    03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(values)
+  total = 0
+  values.each do |i|
+    total = total + i
+  end
+  total
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
-#write your code here
+def shout (word)
+  word.upcase
+end
+
+def echo (word)
+  word
+end
+
+def repeat (word, number = 2)
+  output = word
+  (number - 1).times do
+    output = output + " " + word
+  end
+  output
+end
+
+def start_of_word (word, number = 1)
+  word[0, number]
+end
+
+def first_word (word)
+  word.split[0]
+end
+
+def titleize (title)
+  words = title.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
