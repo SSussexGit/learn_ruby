94488be72dd5a3781cb9946201c7fc48e3453fe5
ScottSussexGit <SSussexGit@users.noreply.github.com>
2017-07-08 15:51:18 -0400

    finish

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
-#write your code here
+def translate_individual (word)
+  while !(word.start_with?('a', 'e', 'i', 'o', 'u'))
+  if word[0..1] == "qu"
+    word = word[2..-1] + word[0..1]
+    else
+      first_letter = word[0]
+      word = word[1..-1]
+      word << first_letter
+    end
+  end
+  word << "ay"
+end
+
+def translate (sentance)
+  sentance.split(' ').map{|x| translate_individual(x)}.join(' ')
+end
+
+
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  attr_reader :time_string
+
+  def initialize
+    @seconds = 0
+  end
+
+  def two_digit (number)
+    output = number.to_s
+    if output[1] == nil
+      output = "0" + output
+    end
+    output
+  end
+
+  def time_string 
+    @time_string = two_digit((seconds/3600)%60) + ":" + two_digit((seconds/60)%60) + ":" + two_digit(seconds%60)
+  end
 end
